4dc62bef7b3bc8aa40aa6cdc5f7597ab2fcbeb1b
davyt <dmelgazi@gmail.com>
2026-07-08 15:01:51 -0300

    debug: log AI reply y actions en webhook

--- app/Http/Controllers/WhatsAppWebhookController.php
+++ app/Http/Controllers/WhatsAppWebhookController.php
@@ -146,6 +146,8 @@ class WhatsAppWebhookController extends Controller
             $reply   = $result['reply'];
             $actions = $result['actions'];
 
+            Log::info('AI reply', ['lead_id' => $lead->id, 'reply' => $reply, 'actions' => $actions]);
+
             if ($agent->auto_send) {
                 $company = $lead->company;
                 if ($company?->wa_active && $company->wa_phone_number_id && $company->wa_access_token) {
